abc9fdc903a41d287c05f2ca81f453bdf501f1b0
leezp <1138088202@qq.com>
2020-06-18 17:46:28 +0800

    日常更新

--- tools/tcp发包/socketclient.py
+++ tools/tcp发包/socketclient.py
@@ -7,7 +7,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 if __name__=="__main__":
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("139.129.89.134", 8080))
+    s.connect(("ip", 8080))
     s.send('''\xabsth.''') # 十六进制数据，字符串等正文
     print s.recv(1024)
     s.close()
